1cf5230508c62edc5d3a9a509c365df64a1a69e8
PerformanceEngineer <64775369+PerformanceEngineer@users.noreply.github.com>
2024-04-24 14:22:45 +0200

    Hand Portions
    Implemented hand portion sizes

--- PACalc/src/components/TheCalc.vue
+++ PACalc/src/components/TheCalc.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const step = ref(1)
 const name = ref('')
@@ -16,10 +16,90 @@ const teehigh = ref(0.0)
 const low = ref(0.0)
 const high = ref(0.0)
 
+// Computed properties for calculating the macronutrient ranges
+const lbm = computed(() => weight.value * (1.0 - bfatestimate.value / 100.0))
+const lowProt = computed(() => Math.round(lbm.value * 1.8))
+const lowFat = computed(() => (teelow.value * 0.2) / 9.0)
+const lowCarb = computed(() => (teelow.value * 0.8 - lowProt.value * 4.5) / 4.5)
+
+const highProt = computed(() => Math.round(lbm.value * 2.2))
+const highFat = computed(() => (teehigh.value * 0.2) / 9.0)
+const highCarb = computed(() => (teehigh.value * 0.8 - highProt.value * 4.5) / 4.5)
+
+let remainingcals = teehigh
+
+const proteinperservingprotein = computed(() => (gender.value === 'male' ? 24.0 : 22.0))
+const kcalperservingprotein = 145
+const protein = computed(() => lbm.value * 2.0)
+
+let proteinservings = 0
+let fatservings = 0
+let carbservings = 0
+let totalcalories = 0
+let totalprotein = 0
+let totalfats = 0
+let totalcarbs = 0
+let veggieservings = 0
+
+function calculateHandSizes() {
+  proteinservings = protein.value / proteinperservingprotein.value
+  remainingcals.value -= proteinservings * kcalperservingprotein
+
+  const fatperservingfat = gender.value === 'male' ? 9.0 : 8.0
+  fatservings = highFat.value / fatperservingfat
+  const kcalperservingfat = 90.0
+  remainingcals.value -= fatservings * kcalperservingfat
+
+  const carbsperservingcarbs = gender.value === 'male' ? 25.0 : 22.0
+  carbservings = highCarb.value / carbsperservingcarbs
+
+  veggieservings = gender.value === 'male' ? 8.0 : 6.0
+
+  if (gender.value === 'male') {
+    proteinservings =
+      0.04369122257053291 * protein.value -
+      0.02155936207914943 * highFat.value -
+      0.0035440047253396337 * highCarb.value
+    carbservings =
+      -0.0049222287851939358 * protein.value -
+      0.002067336089781453 * highFat.value +
+      0.04075605434140578 * highCarb.value
+    fatservings =
+      -0.008663122661941327 * protein.value +
+      0.1163614884819846 * highFat.value -
+      0.0082693443591258121 * highCarb.value
+  } else {
+    proteinservings =
+      0.04768856447688564 * protein.value -
+      0.02092457420924574 * highFat.value -
+      0.0024330900243309 * highCarb.value
+    carbservings =
+      -0.00608272506082725 * protein.value -
+      0.0024330900243309 * highFat.value +
+      0.046228710462287104 * highCarb.value
+    fatservings =
+      -0.0024330900243309 * protein.value +
+      0.116301703163017 * highFat.value -
+      0.0097323600973236 * highCarb.value
+  }
+
+  proteinservings = Math.round(proteinservings)
+  carbservings = Math.round(carbservings)
+  fatservings = Math.round(fatservings)
+
+  totalprotein =
+    proteinservings * 24.0 + carbservings * 3.0 + fatservings * 2.0 + veggieservings * 1.5
+  totalcarbs =
+    proteinservings * 2.0 + carbservings * 22.0 + fatservings * 2.0 + veggieservings * 5.0
+  totalfats = proteinservings * 4.5 + carbservings * 1.0 + fatservings * 8.0
+  totalcalories = totalprotein * 4.5 + totalcarbs * 4.5 + totalfats * 9.0
+}
+
 function nextStep() {
   step.value++
   console.log('step:' + step.value.toString())
   if (step.value == 9) calculateTEE()
+  if (step.value == 11) calculateHandSizes()
 }
 
 function setGender(g) {
@@ -53,25 +133,26 @@ function calculateTEE() {
 
 <template>
   <div class="centerdiv">
-    <div class="logodiv">
+    <div class="MyLogoDiv">
       <img
         id="responsive-image"
-        src="https://highperformers.fit/wp-content/uploads/2021/02/Logo_HighPerformersTrans_Schriftzug.png"
+        src="https://lukaspezenka.files.wordpress.com/2018/02/cropped-header-large-bg.png"
       />
     </div>
   </div>
 
-  <h2 v-if="step === 1">Wie heißt Du?</h2>
-  <h2 v-if="step === 2">Wie alt bist Du?</h2>
-  <h2 v-if="step === 3">Wie hoch ist Dein Gewicht (in kg)?</h2>
-  <h2 v-if="step === 4">Was ist Dein Geschlecht?</h2>
-  <h2 v-if="step === 5">Wie hoch ist Dein Körperfettanteil (in%)?</h2>
-
-  <h2 v-if="step === 6">Was ist Dein Zielgewicht (in kg)?</h2>
-
-  <h2 v-if="step === 7">In der Woche trainierst Du durchschnittlich...?</h2>
-  <h2 v-if="step === 8">Dein Aktivitätsniveau abseits vom Training ist...</h2>
-  <h2 v-if="step === 9">Dein Energiebedarf</h2>
+  <div class="centerdiv">
+    <h2 v-if="step === 1">Wie heißt Du?</h2>
+    <h2 v-if="step === 2">Wie alt bist Du?</h2>
+    <h2 v-if="step === 3">Wie hoch ist Dein Gewicht (in kg)?</h2>
+    <h2 v-if="step === 4">Was ist Dein Geschlecht?</h2>
+    <h2 v-if="step === 5">Wie hoch ist Dein Körperfettanteil (in%)?</h2>
+    <h2 v-if="step === 6">Was ist Dein Zielgewicht (in kg)?</h2>
+    <h2 v-if="step === 7">In der Woche trainierst Du durchschnittlich...?</h2>
+    <h2 v-if="step === 8">Dein Aktivitätsniveau abseits vom Training ist...</h2>
+    <h2 v-if="step === 9">Dein Energiebedarf</h2>
+    <h2 v-if="step === 10">Deine Makronährstoffe</h2>
+  </div>
 
   <form v-if="step === 1" @submit.prevent="nextStep">
     <input class="hugeInput" type="text" v-model="name" placeholder="Dein Name" /><br />
@@ -120,9 +201,14 @@ function calculateTEE() {
     <input class="button" type="submit" value="OK" :disabled="!bfatestimate" />
   </form>
 
-
   <form v-if="step === 6" @submit.prevent="nextStep">
-    <input class="hugeInput" type="number" v-model="targetweight" placeholder="Zielgewicht" min="0" /><br />
+    <input
+      class="hugeInput"
+      type="number"
+      v-model="targetweight"
+      placeholder="Zielgewicht"
+      min="0"
+    /><br />
     <input class="button" type="submit" value="OK" :disabled="!targetweight" />
   </form>
 
@@ -162,25 +248,159 @@ function calculateTEE() {
     </div>
   </form>
 
-  <div v-if="step === 9" class="flex-container">
-    <div class="flex-child" style="border: 0px none"></div>
-    <div class="flex-child">Zielgewicht: {{ targetweight }}</div>
-    <div class="flex-child">Training: {{ traininghours }} Stunden / Woche</div>
-    <div class="flex-child">Kalorienbedarf: {{ teelow }} bis {{ teehigh }} / Tag</div>
-    <div class="flex-child" style="border: 0px none"></div>
+  <div v-if="step === 9" class="centerdiv">
+    <div v-if="step === 9" class="flex-container">
+      <div class="flex-child" style="border: 0px none"></div>
+      <div class="flex-child">Zielgewicht: {{ targetweight }}</div>
+      <div class="flex-child">Training: {{ traininghours }} Stunden / Woche</div>
+      <div class="flex-child">Kalorienbedarf: {{ teelow }} bis {{ teehigh }} / Tag</div>
+      <div class="flex-child" style="border: 0px none"></div>
+    </div>
+
+    <div v-if="step === 9" class="flex-container">
+      <div class="flex-child" style="border: 0px none"></div>
+      <div class="flex-child">
+        Aktivität: {{ neat === 1 ? 'Niedrig' : neat === 2 ? 'Moderat' : 'Hoch' }}
+      </div>
+      <div class="flex-child"></div>
+      <div class="flex-child">
+        Kalorienbedarf:
+        {{ (targetweight * (low + traininghours + neat - 1) * 2.2).toFixed(2) }} kcal / Tag
+      </div>
+      <div class="flex-child" style="border: 0px none"></div>
+    </div>
+
+    <form v-if="step === 9" @submit.prevent="nextStep">
+      <input class="button" type="submit" value="OK" :disabled="!traininghours" />
+    </form>
   </div>
 
-  <div v-if="step === 9" class="flex-container">
-    <div class="flex-child" style="border: 0px none"></div>
-    <div class="flex-child">
-      Aktivität: {{ neat === 1 ? 'Niedrig' : neat === 2 ? 'Moderat' : 'Hoch' }}
-    </div>
-    <div class="flex-child"></div>
-    <div class="flex-child">
-      Kalorienbedarf: {{ (targetweight * (low + traininghours + neat - 1) * 2.2).toFixed(2) }} kcal
-      / Tag
-    </div>
-    <div class="flex-child" style="border: 0px none"></div>
+  <div v-if="step === 10" class="centerdiv">
+    <form @submit.prevent="nextStep">
+      <div class="flex-container" style="background-color: white">
+        <div class="flex-child" style="border: 0px none"></div>
+        <div class="flex-child">
+          <img
+            src="https://cdn1.iconfinder.com/data/icons/fitness-55/300/protein--gym-fitness-sport-512.png"
+            style="width: 100%"
+          /><br />
+          Protein: {{ lowProt }} bis {{ highProt }} Gramm / Tag
+        </div>
+
+        <div class="flex-child">
+          <img src="https://img.icons8.com/ios/452/carbohydrates.png" style="width: 100%" /><br />
+          Kohlenhydrate: {{ lowCarb }} bis {{ highCarb }} Gramm / Tag
+        </div>
+
+        <div class="flex-child">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVv8gHhj3quy7jDCf_9tboeYiZYJqzpjMLoA&usqp=CAU"
+            style="width: 100%"
+          /><br />
+          Fette: {{ lowFat }} bis {{ highFat }} Gramm / Tag
+        </div>
+        <div class="flex-child" style="border: 0px none"></div>
+      </div>
+      <br />
+      <input type="submit" class="button" value="Verstanden" />
+    </form>
   </div>
 
+  <div v-if="step === 11" class="centerdiv">
+    <div class="flex-container" style="margin-bottom: 10px; justify-content: center">
+      <div class="flex-child" style="border: 0px none"></div>
+
+      <div class="flex-child">
+        <div class="arrange-vertically">
+          <div class="flex-container">
+            <div class="flex-child">
+              <img
+                src="https://www.precisionnutrition.com/hand_portion_calculator/assets/images/palm.svg"
+              />
+            </div>
+            <div class="flex-child">
+              Dein täglicher Eiweißbedarf liegt bei {{ proteinservings }} Handtellergroßen
+              Portionen.
+            </div>
+          </div>
+
+          <div class="flex-container">
+            <div class="flex-child">
+              <img
+                src="https://www.precisionnutrition.com/hand_portion_calculator/assets/images/cupped.svg"
+              />
+            </div>
+            <div class="flex-child">
+              Kohlenhydrate befeuern Dein Training und Deine Denkleistung.
+              {{ carbservings }} handvoll (verzehrfertig) sind ein guter Startpunkt.
+            </div>
+          </div>
+
+          <div class="flex-container">
+            <div class="flex-child">
+              <img
+                src="https://www.precisionnutrition.com/hand_portion_calculator/assets/images/thumb.svg"
+              />
+            </div>
+            <div class="flex-child">
+              Gesunde Fette stützen Dein Immunsystem und helfen Dir, mit Entzündungen zurecht zu
+              kommen. : {{ fatservings }} daumengroße Portionen dürfen es schon sein.
+            </div>
+          </div>
+
+          <div class="flex-container">
+            <div class="flex-child">
+              <img
+                src="https://www.precisionnutrition.com/hand_portion_calculator/assets/images/fist.svg"
+              />
+            </div>
+            <div class="flex-child">
+              Eat the Rainbow! Runde Deine Ernährung ab mit {{ veggieservings }} faustgroßen
+              Portionen Gemüse.
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex-child" style="border: 0px none"></div>
+    </div>
+
+    <div class="flex-container" style="margin-bottom: 10px">
+      <div class="flex-child" style="border: 0px none"></div>
+      <div class="flex-child" style="border: 0px none">
+        <div class="arrange-vertically">
+          <div style="margin-bottom: 10px">
+            Bei diesen Portionsvorgaben nimmst Du im Schnitt die folgenden Makronährstoffe zu Dir:
+          </div>
+          <div class="flex-container">
+            <div class="flex-child">
+              <img
+                id="responsive-image"
+                src="https://thumbs.dreamstime.com/z/protein-icon-illustration-white-background-protein-icon-illustration-119365622.jpg"
+              />
+            </div>
+            <div class="flex-child">{{ totalprotein }} Gramm Eiweiß</div>
+          </div>
+
+          <div class="flex-container">
+            <div class="flex-child">
+              <img id="responsive-image" src="https://img.icons8.com/ios/452/carbohydrates.png" />
+            </div>
+            <div class="flex-child">{{ totalcarbs }} Gramm Kohlenhydrate</div>
+          </div>
+
+          <div class="flex-container">
+            <div class="flex-child">
+              <img
+                id="responsive-image"
+                src="https://www.pngitem.com/pimgs/m/163-1631365_trans-fat-no-trans-fat-icon-hd-png.png"
+              />
+            </div>
+            <div class="flex-child">{{ totalfats }} Gramm Fett</div>
+          </div>
+        </div>
+      </div>
+      <div class="flex-child" style="border: 0px none"></div>
+    </div>
+  </div>
 </template>
--- PACalc/src/main.js
+++ PACalc/src/main.js
@@ -1,6 +1,15 @@
+// import Vue from 'vue'
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+//import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Vue.use(BootstrapVue)
+// Vue.use(IconsPlugin)
 
 createApp(App).mount('#app')
